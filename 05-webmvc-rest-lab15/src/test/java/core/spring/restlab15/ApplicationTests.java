7c6ec63b19df555b8b882065e669805bed73a7a7
Богдан Косев <bogdan_gamer@abv.bg>
2021-11-10 20:43:38 +0200

    Initial commit

---
+++ 05-webmvc-rest-lab15/src/test/java/core/spring/restlab15/ApplicationTests.java
@@ -0,0 +1,13 @@
+package core.spring.restlab15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
