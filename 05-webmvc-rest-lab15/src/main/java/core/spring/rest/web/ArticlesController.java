91851e3126bcf9000fe1c29d4527e0ba89e3d0e0
Богдан Косев <bogdan_gamer@abv.bg>
2021-11-17 21:45:27 +0200

    Added exceptions, PUT mapping DELETE mapping not tested

--- 05-webmvc-rest-lab15/src/main/java/core/spring/rest/web/ArticlesController.java
+++ 05-webmvc-rest-lab15/src/main/java/core/spring/rest/web/ArticlesController.java
@@ -1,9 +1,12 @@
 package core.spring.rest.web;
 
+import core.spring.exception.InvalidClientDataException;
 import core.spring.rest.dao.ArticleRepo;
 import core.spring.exception.EntityNotFoundException;
 import core.spring.rest.model.Article;
+import core.spring.rest.model.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -38,5 +41,23 @@ public class ArticlesController {
 //                        .pathSegment("{id}").buildAndExpand(created.getId()).toUri()
         ).body(created);
     }
+
+    @PutMapping("/{id}")
+    public Article updateArticle(@PathVariable String id, @RequestBody Article article) {
+        if(!id.equals(article.getId()))
+            throw new InvalidClientDataException(String.format("ID %s from request URL is different from ID %s in request body.", id, article.getId()));
+        articleRepo.findById(id).orElseThrow(() -> new EntityNotFoundException(String.format("Article with ID %s not found", id)));
+        return articleRepo.save(article);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteArticle(@PathVariable String id) {
+        if(articleRepo.existsById(id)) {
+            articleRepo.deleteById(id);
+        } else{
+            throw new InvalidClientDataException(String.format("ID %s from request URL not found.", id));
+        }
+    }
+
 
 }
