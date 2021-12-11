094db33a850194064566a722c42386e15d7fa92c
Богдан Косев <bogdan_gamer@abv.bg>
2021-12-11 22:04:47 +0200

    Initial commit

---
+++ cooking-recipes-web-mvc/src/main/java/com/cookingrecipes/cookingrecipeswebmvc/CookingRecipesWebMvcApplication.java
@@ -0,0 +1,13 @@
+package com.cookingrecipes.cookingrecipeswebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingRecipesWebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingRecipesWebMvcApplication.class, args);
+    }
+
+}
---
+++ cooking-recipes-web-mvc/src/test/java/com/cookingrecipes/cookingrecipeswebmvc/CookingRecipesWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookingrecipes.cookingrecipeswebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingRecipesWebMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
